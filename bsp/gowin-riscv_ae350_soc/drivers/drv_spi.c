f4d499cef5fdfaba71b758cd0b8f75cf20222da7
llcc01 <2417224420@qq.com>
2024-07-28 21:11:39 +0800

    AE350: Fix SPI CS & Add SD Card

--- bsp/gowin-riscv_ae350_soc/drivers/drv_spi.c
+++ bsp/gowin-riscv_ae350_soc/drivers/drv_spi.c
@@ -13,6 +13,8 @@
 #include "ae350_soc/driver/include/Driver_GPIO.h"
 #include "ae350_soc/lib/gpio.h"
 
+#define SD_CS_PIN 20
+
 struct spi_cs_t {
   rt_uint8_t cs;
   rt_uint8_t pin;
@@ -35,7 +37,23 @@ static rt_ssize_t xfer(struct rt_spi_device *device,
   rt_uint8_t *recv_ptr = message->recv_buf;
   const rt_uint32_t size = message->length;
   
+  // if (send_ptr != RT_NULL)
+  // {
+  //   rt_kprintf("spi send %d ", size);
+  //   for (rt_size_t i = 0; i < size; i++) {
+  //     rt_kprintf("%02x ", send_ptr[i]);
+  //   }
+  //   rt_kprintf("\n");
+  // }
   spi_transfer(AE350_SPI, send_ptr, recv_ptr, size);
+  // if (recv_ptr != RT_NULL)
+  // {
+  //   rt_kprintf("spi read %d ", size);
+  //   for (rt_size_t i = 0; i < size; i++) {
+  //     rt_kprintf("%02x ", recv_ptr[i]);
+  //   }
+  //   rt_kprintf("\n");
+  // }
 
   if (message->cs_release) {
     gpio_write(AE350_GPIO, spi_cs->pin, 1);
@@ -46,16 +64,20 @@ static rt_ssize_t xfer(struct rt_spi_device *device,
 
 static struct rt_spi_bus spi_bus0;
 static struct rt_spi_device spi_device1;
+static struct rt_spi_device spi_device2;
 static struct spi_cs_t spi_cs1 = {.cs = 1, .pin = 0};
+static struct spi_cs_t spi_cs2 = {.cs = 2, .pin = SD_CS_PIN};
 static struct rt_spi_ops spi_ops = {.configure = configure, .xfer = xfer};
 
 int rt_hw_spi_init(void) {
   spi_init(AE350_SPI);
   rt_spi_bus_register(&spi_bus0, "spi0", &spi_ops);
 
-  gpio_mode(AE350_GPIO, spi_cs1.pin, AE350_GPIO_DIR_OUTPUT);
   rt_spi_bus_attach_device(&spi_device1, "spi01", "spi0", &spi_cs1);
+  gpio_mode(AE350_GPIO, spi_cs1.pin, AE350_GPIO_DIR_OUTPUT);
 
+  rt_spi_bus_attach_device(&spi_device2, "spi02", "spi0", &spi_cs2);
+  gpio_mode(AE350_GPIO, spi_cs2.pin, AE350_GPIO_DIR_OUTPUT);
   return 0;
 }
 INIT_BOARD_EXPORT(rt_hw_spi_init);
