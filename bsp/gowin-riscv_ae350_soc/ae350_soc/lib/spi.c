167f30ceae49000fbbb81cb25457e71a96a96d24
llcc01 <2417224420@qq.com>
2024-07-28 21:10:41 +0800

    AE350: fix SPI

--- bsp/gowin-riscv_ae350_soc/ae350_soc/lib/spi.c
+++ bsp/gowin-riscv_ae350_soc/ae350_soc/lib/spi.c
@@ -5,12 +5,15 @@
 #include "ae350.h"
 #include "ae350_soc/driver/ae350/spi_ae350.h"
 
+uint32_t txfifo_size = 0;
+
 void spi_init(SPI_RegDef * spi_base)
 {
-    spi_base->TRANSFMT |= SPI_SLAVE | SPI_MERGE | DATA_BITS(8);
+    spi_base->TRANSFMT |=  SPI_MERGE | DATA_BITS(8);
+    txfifo_size = 2 << ((spi_base->CONFIG >> 4) & 0xf);
 }
 
-void spi_transfer(SPI_RegDef * spi_base, const uint8_t * tx_buf, uint8_t * rx_buf, uint16_t len)
+void spi_transfer_part(SPI_RegDef * spi_base, const uint8_t * tx_buf, uint8_t * rx_buf, uint16_t len)
 {
     uint32_t trans_ctrl = WR_TRANCNT(len) | RD_TRANCNT(len);
     if ((tx_buf == NULL) && (rx_buf != NULL))
@@ -29,26 +32,58 @@ void spi_transfer(SPI_RegDef * spi_base, const uint8_t * tx_buf, uint8_t * rx_bu
     {
         return;
     }
-
     spi_base->TRANSCTRL = trans_ctrl;
+
+    spi_base->CMD = 0;
+
+    spi_base->CTRL = (TXFIFORST | RXFIFORST);
 
     if (tx_buf != NULL)
     {
-        for (size_t i = 0; i < len; i++)
+        for (size_t i = 0; i < len; i += 4)
         {
-            spi_base->DATA = tx_buf[i];
+            spi_base->DATA = *(uint32_t *)(tx_buf + i);
         }
     }
-    spi_base->CMD = 0;
 
-    while (spi_base->STATUS & SPI_STATUS_ACTIVE);
-    
+    while (spi_base->STATUS & 0x1)
+    {
+        ;
+    }
+
     if (rx_buf != NULL)
     {
-        for (size_t i = 0; i < len; i++)
+        for (size_t i = 0; i < len; i += 4)
         {
-            rx_buf[i] = spi_base->DATA;
+            uint32_t d = spi_base->DATA;
+            for (size_t j = 0; (j < 4) && (i + j < len); j++)
+            {
+                rx_buf[i + j] = d & 0xff;
+                d >>= 8;
+            }
         }
     }
 
+}
+
+void spi_transfer(SPI_RegDef * spi_base, const uint8_t * tx_buf, uint8_t * rx_buf, uint16_t len)
+{
+    uint32_t part_len = txfifo_size;
+    while (len > 0)
+    {
+        if (len < part_len)
+        {
+            part_len = len;
+        }
+        spi_transfer_part(spi_base, tx_buf, rx_buf, part_len);
+        len -= part_len;
+        if (tx_buf != NULL)
+        {
+            tx_buf += part_len;
+        }
+        if (rx_buf != NULL)
+        {
+            rx_buf += part_len;
+        }
+    }
 }
